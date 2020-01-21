b686c6c146bafee66e9c93daab11afb54a2c9b76
kastori <kastori1990@gmail.com>
2020-01-21 22:56:12 +0900

    algo

--- swea/d2/swea1926.java
+++ swea/d2/swea1926.java
@@ -6,17 +6,30 @@ public class swea1926 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();	
-
-		for(int test_case = 1; test_case <= T; test_case++){
-			int N = sc.nextInt();
-			String result = "";
-			for(int i =1 ; i<= N; i++) {
-				
+		
+		int N = sc.nextInt();
+		String result ="";
+		for(int i =1 ; i<= N; i++) {
+			int num = i;
+			boolean chk = false;
+			
+			while(num!=0) {
+				int n = num % 10;
+				num /=10;
+				if(n!=0 && n%3==0) {
+					chk = true;
+					result +="-";				
+				}				
 			}
-
+			if(chk) {
+				result += " ";
+			}else {
+				result += i + " ";
+			}
+			
 		}
+
+		System.out.println(result);
 	}
 
 }
--- swea/d2/swea1954.java
+++ swea/d2/swea1954.java
@@ -1,10 +1,71 @@
 package swea.d2;
 
+import java.util.Scanner;
+
+//https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PobmqAPoDFAUq&categoryId=AV5PobmqAPoDFAUq&categoryType=CODE
+
 public class swea1954 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int T,N,cnt,size,x,y,startX,startY,endX,endY, direction;
+		T=sc.nextInt();		
+		int [][]a;
+		
+		for(int test_case = 1; test_case <= T; test_case++){
+			N = sc.nextInt();
+			a = new int[N][N];
+			cnt = 1;			
+			x = 0; y =0; startX = 0; startY = 0; endX=N-1; endY=N-1;
+			direction = 0; // 우 하 좌 상
+			for(int i =0 ;i<(N*N);i++) {				
+				a[y][x] = cnt++;
+				switch(direction) {
+				case 0:
+					if(x!=endX) {
+						x++;
+					}else {
+						direction++;
+						startY++;
+						y++;
+					}
+					break;
+				case 1:
+					if(y!=endY) {
+						y++;
+					}else {
+						direction++;
+						endX--;
+						x--;
+					}
+					break;
+				case 2:
+					if(x!=startX) {						
+						x --;
+					}else {
+						direction++;
+						endY--;
+						y--;
+					}
+					break;
+				case 3:
+					if(y!=startY) {
+						y--;
+					}else {
+						direction=0;
+						startX++;
+						x++;
+					}
+					break;
+				}				
+			}
+			System.out.println("#"+test_case);
+			for(int i=0;i<N;i++) {
+				for(int j =0;j<N;j++) {
+					System.out.print(a[i][j]+" ");
+				}
+				System.out.println();
+			}
+		}
 	}
 
 }
