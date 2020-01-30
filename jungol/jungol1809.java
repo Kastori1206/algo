85c4b986c308a87ee4782339db0af183e32419cb
kastori <kastori1990@gmail.com>
2020-01-30 18:05:39 +0900

    2020-01-30

---
+++ jungol/jungol1809.java
@@ -0,0 +1,52 @@
+package jungol;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+//http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=1082&sca=3020
+class pair{
+	int high;
+	int idx;
+	pair(int h, int i){
+		high = h;
+		idx = i;
+	}
+}
+
+public class jungol1809 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();		
+		
+		Stack<pair> st = new Stack<pair>();
+		int [] a = new int[N+1];
+		for(int i =1;i<=N;i++) {
+			int h = sc.nextInt();
+			
+			if(st.isEmpty()) {
+//				a[i] =0;
+				System.out.print(0+" ");
+			}else {
+				while(!st.empty()) {
+					if(st.peek().high<h) {
+						st.pop();
+						if(st.isEmpty()) {
+//							a[i]=0;
+							System.out.print(0+" ");
+						}
+					}else {
+//						a[i] = st.peek().idx;
+						System.out.print(st.peek().idx+" ");
+						break;
+					}
+				}
+			}
+			st.push(new pair(h,i));
+		}
+		
+//		for(int i =1;i<=N;i++) {
+//			System.out.print(a[i]+" ");
+//		}
+//		
+	}
+}
