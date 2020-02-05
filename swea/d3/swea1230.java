cb704871564614d950922ff28c6c430a981ed248
kastori <kastori1990@gmail.com>
2020-02-05 17:56:55 +0900

    2020.02.05

--- swea/d3/swea1230.java
+++ swea/d3/swea1230.java
@@ -3,14 +3,14 @@ package swea.d3;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class swea1229 {
+public class swea1230 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		for(int test_case = 1 ;test_case<=10;test_case++) {
 			int N = sc.nextInt(); //원본 암호문의 길이
 			
-			LinkedList<Integer> list = new LinkedList<>();//원본암호문
+			LinkedList<Integer> list = new LinkedList<>();
 			
 			for(int i =0;i<N;i++) {
 				list.add(sc.nextInt());
@@ -19,23 +19,34 @@ public class swea1229 {
 			int M = sc.nextInt(); //명령어의 개수
 			for(int i= 0 ; i<M;i++) {
 				String command = sc.next();
-				int index = sc.nextInt();
-				int size = sc.nextInt();
-				if(command.equals("I")) {			
+				switch (command) {
+				case "I":
+					int index = sc.nextInt();
+					int size = sc.nextInt();
 					for(int j = 0; j<size;j++) {
 						int temp = sc.nextInt();
-						list.add(index, temp);
-						index++;
+						list.add(index++, temp);
 					}
-				}else if(command.equals("D")) {				
+					break;
+				case "D":
+					index = sc.nextInt();
+					size = sc.nextInt();
 					for(int j =0;j<size;j++) {
 						list.remove(index);
 					}
-				}				
+					break;
+				case "A":
+					size = sc.nextInt();
+					for(int j=0;j<size;j++) {
+						int temp =sc.nextInt();
+						list.add(temp);
+					}					
+					break;
+				}
 			}
 			System.out.print("#"+ test_case+" ");
-			for(int j=0;j<10;j++) {
-				System.out.println(list.get(j));
+			for(Integer n : list) {
+				System.out.print(n +" ");
 			}
 			System.out.println();
 		}
