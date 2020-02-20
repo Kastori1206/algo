db160c080e8ad3b834379ef7738a6c6bc8ad0a6e
kastori <kastori1990@gmail.com>
2020-02-20 18:00:16 +0900

    baeck170204

---
+++ baek/baek17204.java
@@ -0,0 +1,14 @@
+package baek;
+
+import java.util.Scanner;
+
+public class baek17204 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] arr = new int[N];
+		
+	}
+}
