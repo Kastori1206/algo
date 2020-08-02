87b935f97930f720be97b592cd8a91c0c6a2df26
Kim Young Min <kastori1990@gmail.com>
2020-08-02 18:45:39 +0900

    swea4530. 극한의 청소 작업

---
+++ swea/d4/swea4530.java
@@ -0,0 +1,21 @@
+package swea.d4;
+/*
+ * D4 4530.극한의 청소 작업
+ */
+import java.util.Scanner;
+
+public class swea4530 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int t = 1;t<=T;t++) {
+			long A = sc.nextLong();
+			long B = sc.nextLong();
+			
+			
+			
+		}
+	}
+	 
+}
