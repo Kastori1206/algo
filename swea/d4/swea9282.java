071b51f1a387b343847e1856f9be5b3454d84aa4
Kim Young Min <kastori1990@gmail.com>
2020-08-02 18:46:24 +0900

    swea9282. 초콜릿과 건포도

---
+++ swea/d4/swea9282.java
@@ -0,0 +1,88 @@
+package swea.d4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+ * [D4] 9282. 초콜릿과 건포도
+ */
+public class swea9282 {
+	static int result;
+	static int n,m;	//행, 열 갯수
+	static int[][] choco;
+	static int[][][][] dp; //x,y,h,w
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T= sc.nextInt();
+		
+		for(int t = 1 ;t<=T;t++) {
+			n = sc.nextInt();
+			m = sc.nextInt();
+			
+			choco = new int[n][m];
+			dp = new int[n+1][m+1][n+1][m+1];
+			for(int[][][] d1 : dp) {
+				for(int[][] d2 : d1) {
+					for(int[] d3 :d2) {
+						Arrays.fill(d3, Integer.MAX_VALUE);
+					}
+				}
+			}
+			for(int i =0;i<n;i++) {
+				for(int j = 0;j<m;j++) {
+					choco[i][j] = sc.nextInt();
+				}
+			}
+			result = dfs(0,0,n,m);
+			
+			System.out.printf("#%d %d\n",t,result);
+		}
+	}
+	static int dfs(int x, int y, int h, int w) {
+//		종료
+		if(w == 1 && h ==1) {
+			return 0;
+		}
+		if(dp[y][x][h][w] != Integer.MAX_VALUE) {
+			return dp[y][x][h][w];
+		}
+//		실행
+//		기존에 있던 덩어리의 건포도 개수
+		int sum =0;
+		for(int i =y;i<y+h;i++) {
+			for(int j=x;j<x+w;j++) {
+				sum +=choco[i][j];
+			}
+		}
+//		가로로 나누어서 비용을 최소비용을 구한다.
+		for(int i=1;i<h;i++) {
+//			위쪽 비용
+			if(dp[y][x][i][w]==Integer.MAX_VALUE) {
+				dp[y][x][i][w] = dfs(x,y,i,w);				
+			}
+//			아래쪽 비용
+			if(dp[y+i][x][h-i][w] == Integer.MAX_VALUE) {
+				dp[y+i][x][h-i][w] = dfs(x,y+i,h-i,w);				
+			}
+			int sum3 = sum + dp[y][x][i][w] + dp[y+i][x][h-i][w];
+			dp[y][x][h][w] = Math.min(dp[y][x][h][w], sum3);
+		}
+//		세로로 나누어서 비용을 최소비용을 구한다.
+		for(int i =1;i<w;i++) {
+//			왼쪽비용
+			if(dp[y][x][h][i] == Integer.MAX_VALUE) {
+				dp[y][x][h][i] = dfs(x,y,h,i);				
+			}
+//			오른쪽비용
+			if(dp[y][x+i][h][w-i] == Integer.MAX_VALUE) {
+				dp[y][x+i][h][w-i] = dfs(x+i,y,h,w-i);				
+			}
+			int sum3 = sum+ dp[y][x][h][i] + dp[y][x+i][h][w-i];
+			dp[y][x][h][w] = Math.min(dp[y][x][h][w], sum3);
+		}
+//		재귀호출
+		
+		
+		return dp[y][x][h][w];
+	}
+}
