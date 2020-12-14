e97f60571910eeed1308a4343031efbe70cce2f0
Kim Young Min <kastori1990@gmail.com>
2020-12-14 21:57:58 +0900

    baek5549. 행성탐사

--- baek/baek5549.java
+++ baek/baek5549.java
@@ -3,73 +3,74 @@ package baek;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
+/*
+ * https://www.acmicpc.net/problem/5549
+ * 행성탐사
+ * DP + 구간합
+ */
 public class baek5549 {
-	static int[] dx = {0, 0, -1, 1};
-	static int[] dy = {-1, 1, 0, 0};
-	static int[] result;
-	
+	static int[][] jungle, osean, ice;
+	static int N, M, K;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-	
-		int N = Integer.parseInt(st.nextToken()); //세로
-		int M = Integer.parseInt(st.nextToken()); //가로
-		
-		char[][] map = new char[N+1][M+1];
-		int K = Integer.parseInt(br.readLine());
 
-		int[][] v;
-		for(int i=1;i<=N;i++) {
+		N = Integer.parseInt(st.nextToken()); // 세로
+		M = Integer.parseInt(st.nextToken()); // 가로
+
+		K = Integer.parseInt(br.readLine()); //좌표수
+
+		ice = new int[N + 1][M + 1];
+		jungle = new int[N + 1][M + 1];
+		osean = new int[N + 1][M + 1];
+
+		//각각 수를 저장
+		for (int i = 1; i <= N; i++) {
 			String temp = br.readLine();
-			for(int j=1;j<=M;j++) {
-				map[i][j] = temp.charAt(j-1);
-			}			
-		}		
+			for (int j = 1; j <= M; j++) {
+				if (temp.charAt(j - 1) == 'J') {
+					jungle[i][j]++;
+				} else if (temp.charAt(j - 1) == 'O') {
+					osean[i][j]++;
+				} else if (temp.charAt(j - 1) == 'I') {
+					ice[i][j]++;
+				}
+			}
+		}
 		
-		for(int t = 0 ; t<K;t++) {
-			result = new int[3]; //J, O, I
-			v = new int[N+1][M+1];
-			int[] arr = new int[4];
-			String[] temp = br.readLine().split(" ");
-			for(int i=0;i<4;i++) {
-				arr[i] = Integer.parseInt(temp[i]);
+		//누적 합 구하기
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				jungle[i][j] += jungle[i][j - 1];
+				osean[i][j] += osean[i][j - 1];
+				ice[i][j] += ice[i][j - 1];
 			}
+		}
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				jungle[i][j] += jungle[i-1][j];
+				osean[i][j] += osean[i-1][j];
+				ice[i][j] += ice[i-1][j];
+			}
+		}
+		
+		//결과 구하기
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
 			
-			dfs(map,v,arr,arr[1],arr[0]);
-			for(int i=0;i<3;i++) {
-				System.out.print(result[i]+" ");
-			}
-			System.out.println();			
-		}		
-	}
-	static void dfs(char[][] map, int[][] v,int[] arr, int x,int y) {
-		v[y][x] = 1;
-		if(map[y][x] =='J') {
-			result[0]++;
-		}else if(map[y][x] =='O') {
-			result[1]++;
-		}else {
-			result[2]++;
+			int J = jungle[y2][x2] - jungle[y1-1][x2] - jungle[y2][x1-1] + jungle[y1-1][x1-1];
+			int O = osean[y2][x2] - osean[y1-1][x2] - osean[y2][x1-1] + osean[y1-1][x1-1];
+			int I = ice[y2][x2] - ice[y1-1][x2] - ice[y2][x1-1] + ice[y1-1][x1-1];
+			
+			System.out.println(J+" " + O + " " + I);
 		}
-		for(int i =0;i<4;i++) {
-			int nx = x+dx[i];
-			int ny = y+dy[i];
-			if(!isBound(nx,ny,arr[1],arr[0],arr[3],arr[2])) {
-				continue;
-			}
-			if(v[ny][nx] ==1) {
-				continue;
-			}			
-			dfs(map,v,arr,nx,ny);
-		}
-	}
-	static boolean isBound(int x, int y, int x1, int y1,int x2, int y2) {
-		if(x < x1 || y<y1 || x>x2 || y>y2 ) {
-			return false;
-		}
-		return true;
 	}
 }
