c33327bc4ba04e7a932ffad7887928e72be25dfe
Kim Young Min <kastori1990@gmail.com>
2020-12-14 21:57:33 +0900

    baek2589. 보물섬

--- baek/baek2589.java
+++ baek/baek2589.java
@@ -2,98 +2,100 @@ package baek;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 /*
- * 2589. 보물섬
+ * 2589. 보물섬 
  * https://www.acmicpc.net/problem/2589
+ * BFS
  */
 
 public class baek2589 {
-	static int[] dx = {0, 0, -1, 1};
-	static int[] dy = {-1, 1, 0, 0};
-	static boolean[][] v;
-	static int[][] dist;
-	static char[][] map;
-	static Queue<Pair> q;
-	static int ans;
-	
-	static class Pair{
-		int x; 
-		int y;
-		Pair(int x, int y){
-			this.x = x;
-			this.y = y;
+	static public class Point {
+		int r, c;
+
+		public Point(int r, int c) {
+			this.r = r;
+			this.c = c;
 		}
 	}
-	public static void main(String[] args) throws Exception{
+
+	static int R, C, ans;
+	static int dist[][];
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static char[][] map;
+	static boolean[][] visited;
+	static List<Point> lands = new ArrayList<>();
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		map = new char[n][m];
-		
+
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+
+		map = new char[R][C];
 		ans = 0;
 		
-		for(int i =0;i<n;i++) {
-			map[i] = br.readLine().toCharArray();
+		for (int r = 0; r < R; r++) {
+			map[r] = br.readLine().toCharArray();
+			for(int c = 0 ;c<C;c++) {
+				if(map[r][c]=='L') {
+					lands.add(new Point(r,c));
+				}
+			}
 		}
 		
-		for(int i=0;i<n;i++) {
-			for(int j =0 ;j<m;j++) {
-				if(map[i][j] != 'L') {
-					continue;
-				}
-				v =  new boolean[n][m];
-				dist = new int[n][m];
-				q = new LinkedList<>();				
-				q.add(new Pair(j,i));				
-				int temp =0;					
-				
-				while(!q.isEmpty()) {
-					Pair p= q.poll();
-					int x = p.x;
-					int y = p.y;
-					
-					for(int k=0;k<4;k++) {
-						int nx = x + dx[k];
-						int ny = y + dy[k];
-						
-						if(!isBound(nx, ny, n, m)) {
-							continue;
-						}
-						if(v[ny][nx]) {
-							continue;
-						}
-						if(map[ny][nx] !='L') {
-							continue;
-						}
-						q.add(new Pair(nx,ny));
-						dist[ny][nx] = dist[y][x]+1;
-						v[ny][nx] = true;
-						
-						if(temp<dist[ny][nx]) {
-							temp = dist[ny][nx];
-						}
-					}
-				}
-				if(ans <temp) {
-					ans =temp;
-				}
-			}			
+		for(int i =0;i<lands.size();i++) {
+			Point start = lands.get(i);
+			bfs(start);
 		}
+
 		System.out.println(ans);
 	}
-	
-	static boolean isBound(int x, int y, int n , int m) {
-		if(x<0|| y<0||x>=m||y>=n) {
-			return false;
+
+	public static void bfs(Point start) {
+		Queue<Point> q = new LinkedList<>();
+		visited = new boolean[R][C];
+		dist = new int[R][C];
+		
+		q.add(start);
+		visited[start.r][start.c] = true;
+		
+		int temp  = 0;
+		while (!q.isEmpty()) {
+			Point p = q.poll();
+
+			for (int d = 0; d < 4; d++) {
+				int nr = p.r + dr[d];
+				int nc = p.c + dc[d];
+
+				if (!isIn(nr, nc)) {
+					continue;
+				}
+				if (visited[nr][nc]) {
+					continue;
+				}
+				if (map[nr][nc] == 'W') {
+					continue;
+				}
+				visited[nr][nc] = true;
+				dist[nr][nc] = dist[p.r][p.c]+1;
+				q.add(new Point(nr, nc));
+				temp = Math.max(temp, dist[nr][nc]);
+			}
 		}
-		return true;
+		
+		ans = Math.max(ans, temp);
+		
 	}
-	
+
+	public static boolean isIn(int r, int c) {
+		return r >= 0 && c >= 0 && r < R && c < C;
+	}
+
 }
