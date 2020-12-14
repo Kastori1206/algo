692e9762788dff34695f4c3a5c22d46cf8179fd6
Kim Young Min <kastori1990@gmail.com>
2020-12-14 21:58:55 +0900

    baek14889. 스타트와 링크

---
+++ baek/baek14889.java
@@ -0,0 +1,79 @@
+package baek;
+/*
+ * 브루트포스 - 재귀
+ * https://www.acmicpc.net/problem/15661
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class baek14889 {
+	static int N;
+	static int[][] persons;
+	static boolean[] visited;
+	static int answer;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		
+		persons = new int[N][N];
+		visited = new boolean[N];
+		answer = Integer.MAX_VALUE;
+		
+		for(int r = 0; r<N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for(int c = 0;c <N;c++) {
+				persons[r][c] = Integer.parseInt(st.nextToken());
+			}
+		}		
+		dfs(0);
+		System.out.println(answer);
+	}
+	private static void dfs(int index) {
+		if(index==N) {
+			//공집합 체크
+			if(check()) {
+				//능력치 계산 비교
+				calc();				
+			}
+			return;
+		}
+		
+		visited[index] = true;
+		dfs(index+1);
+		visited[index] = false;
+		dfs(index+1);
+		
+	}
+	private static boolean check() {
+		boolean flag = visited[0];
+		for(int i = 1; i<N;i++) {
+			if(flag != visited[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	private static void calc() {		
+		
+		int teamStart=0, teamLink=0;	
+		for(int i =0;i<N;i++) {
+			for(int j=i+1;j<N;j++) {
+				//i번째 사람과 j 번째 사람이 true라면 스타트팀 점수
+				if(visited[i] && visited[j]) {
+					teamStart += persons[i][j] + persons[j][i];
+				}
+				//i번째 사람과 j 번째 사람이 false라면 링크팀 점수
+				else if(!visited[i] && !visited[j]) {
+					teamLink += persons[i][j] + persons[j][i];
+				}
+			}
+		}
+		
+		answer = Math.min(answer, Math.abs(teamStart-teamLink));
+	}
+}
