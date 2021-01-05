e1c8382421d93cc2666c110405beb9f07f41340c
김영민 <kastori1990@gmail.com>
2021-01-06 02:24:36 +0900

    codeforces round 694(div2). B - Strange List

---
+++ codeforecs/div2/round694/B.java
@@ -0,0 +1,51 @@
+package codeforecs.div2.round694;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B {
+	public static void main(String[] args) throws Exception{ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 0;t<T;t++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int[] arr = new int[n];
+			
+			st = new StringTokenizer(br.readLine());
+			long ans = 0;
+			int[] dp = new int[n];
+			
+			for(int i =0;i<n;i++) {				
+				arr[i] = Integer.parseInt(st.nextToken());
+				int temp  = arr[i];
+				ans += temp;
+				while(temp %x==0) {
+					temp /= x;					
+					dp[i]++;
+				}
+				
+			}
+			boolean flag = false;
+			while(true) {
+				for(int i=0;i<n;i++) {
+					if(dp[i] ==0) {
+						flag = true;
+						break;
+					}
+					ans+=arr[i];
+					dp[i]--;
+				}
+				if(flag) {
+					break;
+				}
+			}
+			System.out.println(ans);
+		}
+		
+	}
+}
