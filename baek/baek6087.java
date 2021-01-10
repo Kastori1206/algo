963d5f5cef6e9ce43d24afefc9a4e5fdd648a4d7
김영민 <kastori1990@gmail.com>
2021-01-11 00:18:12 +0900

    baek6087. 레이저 통신

--- baek/baek6087.java
+++ baek/baek6087.java
@@ -3,29 +3,34 @@ package baek;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
+/*
+ * 레이저 통신
+ * https://www.acmicpc.net/problem/6087
+ * BFS
+ */
 public class baek6087 {
 	static int[] dr = {-1,1,0,0};
 	static int[] dc = {0,0,-1,1};
 	static char[][] map;
 	static int[][] dist;
-	static boolean[][] visited;
-	static int R , C , result;
-	static class Point{
-		int r, c, d;
-		public Point(int r, int c, int d) {
+	static int R , C ;
+	static class Point implements Comparable<Point>{
+		int r, c, d, cnt;
+		public Point(int r, int c, int d, int cnt) {
 			super();
 			this.r = r;
 			this.c = c;
 			this.d = d;			
+			this.cnt = cnt;
 		}
-	
-		
+		@Override
+		public int compareTo(Point o) {
+			// TODO Auto-generated method stub
+			return Integer.compare(this.cnt, o.cnt);
+		}		
 	
 	}
 	public static void main(String[] args) throws Exception{
@@ -37,38 +42,31 @@ public class baek6087 {
 		
 		map = new char[R][C];
 		dist =new int[R][C];		
-		visited = new boolean[R][C];
 		Point[] points = new Point[2];
-		int cnt = 0;
+		
+		int cnt =0;
 		for(int r=0;r<R;r++) {
-			Arrays.fill(dist[r], -1);
+			Arrays.fill(dist[r], R*C);
 			map[r] = br.readLine().toCharArray();			
 			for(int c = 0; c<C;c++) {
 				if(map[r][c] == 'C') {
-					points[cnt++] = new Point(r,c,-1);					
+					points[cnt++] = new Point(r,c,-1,0);					
 				}
 			}			
 		}		
 		
-		bfs(points[0],points[1]);
-		System.out.println(result);
-		for(int[] a : dist) {
-			for(int b : a) {
-				System.out.printf("%3d ",b);
-			}
-			System.out.println();
-		}
+		System.out.println(bfs(points[0],points[1]));
 	}
-	private static void bfs(Point start, Point end) {
-		Queue<Point> q = new LinkedList<>();
+	private static int bfs(Point start, Point end) {
+		PriorityQueue<Point> q = new PriorityQueue<>();
 		q.add(start);
 		dist[start.r][start.c] = 0;
+		
 		while(!q.isEmpty()) {
 			Point p = q.poll();
-//			System.out.println(p);
-			if(p.r == end.r && p.c == end.c) {
-				//연결
-				return;
+
+			if(p.r == end.r && p.c == end.c) {				
+				return p.cnt;
 			}
 			
 			for(int d = 0;d<4;d++) {
@@ -82,16 +80,22 @@ public class baek6087 {
 				if(map[nr][nc] == '*') {
 					continue;
 				}
-				if(p.d == -1) {
-					p.d =d;
-					dist[nr][nc] = dist[p.r][p.c];
-				}else if(p.d != d) {					
-					p.d = d;
-					dist[nr][nc] = dist[p.r][p.c]+1; 
-				}				
-				q.offer(new Point(nr,nc,d));				
+				
+				if(p.d == d || p.d == -1) {
+					if(dist[nr][nc] >= p.cnt) {
+						dist[nr][nc] = p.cnt;
+						q.offer(new Point(nr,nc,d,p.cnt));
+					}
+				}else {
+					if(dist[nr][nc]>= p.cnt +1) {
+						dist[nr][nc] = p.cnt +1;
+						q.add(new Point(nr, nc,d,p.cnt+1));
+					}
+				}
+				
 			}
 		}
+		return -1;
 	}
 	private static boolean isIn(int r, int c) {
 		return r>=0 && c>=0 && r<R && c<C;
