6e3c6b670240be067893e300f0779f698705422e
김영민 <kastori1990@gmail.com>
2021-01-18 22:16:00 +0900

    baek2251. 물통

--- baek/baek2251.java
+++ baek/baek2251.java
@@ -3,6 +3,7 @@ package baek;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -13,8 +14,8 @@ import java.util.StringTokenizer;
  * https://www.acmicpc.net/problem/2251
  */
 public class baek2251 {
-	static class Water{
-		int a,b,c;
+	static class Water {
+		int a, b, c;
 
 		public Water(int a, int b, int c) {
 			super();
@@ -27,86 +28,88 @@ public class baek2251 {
 		public String toString() {
 			return "water [a=" + a + ", b=" + b + ", c=" + c + "]";
 		}
-		
+
 	}
-	static boolean[][] visited;
+
+	static boolean[][][] visited;
 	static List<Integer> answer = new ArrayList<>();
-	static int A,B,C;
-	public static void main(String[] args) throws Exception{
+	static int A, B, C;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		A = Integer.parseInt(st.nextToken());
 		B = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		
-		int N =Math.max(Math.max(A, B),C);
-		
-		visited = new boolean[3][N];
-		
-		bfs(new Water(0,0,C));
+
+		visited = new boolean[A + 1][B + 1][C + 1];
+
+		bfs(new Water(0, 0, C));
+
+		Collections.sort(answer);
+		for (Integer n : answer) {
+			System.out.print(n + " ");
+
+		}
 	}
+
 	private static void bfs(Water water) {
 		Queue<Water> q = new LinkedList<>();
-		visited[3][water.c] = true;
-		
-		while(!q.isEmpty()) {
+		q.add(water);
+		while (!q.isEmpty()) {
 			Water w = q.poll();
-			
-			if(w.a == 0) {
+			int a = w.a;
+			int b = w.b;
+			int c = w.c;
+
+			if (visited[a][b][c]) {
+				continue;
+			}
+			if (w.a == 0) {
 				answer.add(w.c);
 			}
-			for(int d =0;d<3;d++) {				
-				//A의 물통 옮김
-				int temp;
-				if(d == 0) {
-					if(visited[d][w.a]) {
-						continue;
-					}
-					visited[d][w.a] = true;
-					//B로 옮김
-					temp = B - w.b + w.a; 
-					if(temp >B) {						
-						q.offer(new Water(w.a-(temp-B),temp -B,w.c));
-					}else {
-						q.offer(new Water(0, temp, w.c));
-					}
-					//C로 옮김
-					temp = B - w.b + w.a;
-					if(temp >B) {						
-						q.offer(new Water(w.a-(temp-B),temp -B,w.c));
-					}else {
-						q.offer(new Water(0, temp, w.c));
-					}
-				}
-				//B의 물통 옮김
-				if(d == 1) {
-					int temp = B - w.b + w.a; 
-					visited[d][w.a] = true;
-					//A로 옮김
-					if(temp >B) {						
-						q.offer(new Water(w.a-(temp-B),temp -B,w.c));
-					}
-					//C로 옮김
-					else {
-						q.offer(new Water(0, temp, w.c));
-					}
-				}
-				//C의 물통 옮김
-				if(d == 2) {
-					int temp = B - w.b + w.a; 
-					visited[d][w.a] = true;
-					//A로 옮김
-					if(temp >B) {						
-						q.offer(new Water(w.a-(temp-B),temp -B,w.c));
-					}
-					//B로 옮김
-					else {
-						q.offer(new Water(0, temp, w.c));
-					}
-				}
+			visited[a][b][c] = true;
+			// 물통 A로 물 이동
+			if (a + b <= A) {
+				q.add(new Water(a + b, 0, c));
+			} else {
+				q.add(new Water(A, a + b - A, c));
+			}
+
+			if (a + c <= A) {
+				q.add(new Water(a + c, b, 0));
+			} else {
+				q.add(new Water(A, b, a + c - A));
+			}
+
+			// 물통 B로 물 이동
+			if (b + a <= B) {
+				q.add(new Water(0, a + b, c));
+			} else {
+				q.add(new Water(a + b - B, B, c));
+			}
+
+			if (b + c <= B) {
+				q.add(new Water(a, b + c, 0));
+			} else {
+				q.add(new Water(a, B, b + c - B));
+			}
+
+			// 물통 C로 물 이동
+
+			if (c + a <= C) {
+				q.add(new Water(0, b, c + a));
+			} else {
+				q.add(new Water(c + a - C, b, C));
+			}
+
+			if (c + b <= C) {
+				q.add(new Water(a, 0, c + b));
+			} else {
+				q.add(new Water(a, c + b - C, C));
 			}
 		}
-		
+
 	}
 
 }
