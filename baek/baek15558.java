92bfbff446942a7d6b180f79d53649156393797b
Kim Young Min <kastori1990@gmail.com>
2021-01-20 15:40:38 +0900

    baek15558. 점프게임

--- baek/baek15558.java
+++ baek/baek15558.java
@@ -6,11 +6,14 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/* 점프게임
+ * https://www.acmicpc.net/problem/15558
+ */
 public class baek15558 {
-	static class Node{
-		int r, c,s;
+	static class Node {
+		int r, c, s; // r = 지도 번호 0 ,1 / c = 칸의 위치 / s = 경과시간
 
-		public Node(int r, int c,int s) {
+		public Node(int r, int c, int s) {
 			super();
 			this.r = r;
 			this.c = c;
@@ -19,34 +22,35 @@ public class baek15558 {
 
 		@Override
 		public String toString() {
-			return "Node [r=" + r + ", c=" + c + "]";
+			return "Node [r=" + r + ", c=" + c + ", s=" + s + "]";
 		}
-		
+
 	}
+
 	static boolean[][] visited;
-	static int N , K , answer, MAX = 200001;
-	
-	public static void main(String[] args) throws Exception{
+	static int N, K, answer, MAX = 200001;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		
+
 		visited = new boolean[2][MAX];
 		answer = 0;
-		
-		for(int i =0;i<2;i++) {
+
+		for (int i = 0; i < 2; i++) {
 			String temp = br.readLine();
-			for(int j =1;j<=N;j++) {
-				if(temp.charAt(j-1)=='0') {
+			for (int j = 1; j <= N; j++) {
+				if (temp.charAt(j - 1) == '0') {
 					visited[i][j] = true;
 				}
 			}
-		}		
-		
-		bfs(new Node(0,1,1));
-		
+		}
+
+		bfs(new Node(0, 1, 1));
+
 		System.out.println(answer);
 	}
 
@@ -54,37 +58,34 @@ public class baek15558 {
 		Queue<Node> q = new LinkedList<>();
 		q.add(start);
 		visited[start.r][start.c] = true;
-		while(!q.isEmpty()) {
+		while (!q.isEmpty()) {
 			Node node = q.poll();
 			int r = node.r;
 			int c = node.c;
 			int s = node.s;
-			if(c>= N ) {
+			if (c >= N) {
 				answer = 1;
 				return;
 			}
-			
-			//한칸 앞으로
-			if(c+1 <=N && !visited[r][c+1] && node.s < MAX) {
-				visited[r][c+1] = true;
-				q.add(new Node(r,c+1,s+1));
+
+			// 한칸 앞으로
+			if (c + 1 <= MAX && !visited[r][c + 1] && c + 1 > s) {
+				visited[r][c + 1] = true;
+				q.add(new Node(r, c + 1, s + 1));
 			}
-			
-			//한칸 뒤로
-			if(c-1 >0 && !visited[r][c-1] && node.s <=c-1) {
-				visited[r][c-1] =true;
-				q.add(new Node(r,c-1,s+1));
+
+			// 한칸 뒤로
+			if (c - 1 > 0 && !visited[r][c - 1] && c - 1 > s) {
+				visited[r][c - 1] = true;
+				q.add(new Node(r, c - 1, s + 1));
 			}
-			
-			//반대로 점프
-			r = r == 0 ? 1: 0;
-			if(c+K <=N && !visited[r][c+K] && c+K <=MAX) {
-				visited[r][c+K] = true;
-				q.add(new Node(r,c+K,s+1));
+
+			// 반대로 점프
+			r = r == 0 ? 1 : 0;
+			if (c + K <= MAX && !visited[r][c + K] && c + K > s) {
+				visited[r][c + K] = true;
+				q.add(new Node(r, c + K, s + 1));
 			}
-			
 		}
-		
-		
 	}
 }
