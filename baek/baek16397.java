0e70f8aea78100c82cd1d9f522b5b3cc8de67311
김영민 <kastori1990@gmail.com>
2021-02-04 00:50:55 +0900

    baek16397. 탈출

--- baek/baek16397.java
+++ baek/baek16397.java
@@ -11,62 +11,86 @@ import java.util.StringTokenizer;
  * https://www.acmicpc.net/problem/16397
  */
 public class baek16397 {
-	static class Point{
+	static class Point {
 		int n, cnt;
 
 		public Point(int n, int cnt) {
 			super();
 			this.n = n;
 			this.cnt = cnt;
-		}		
+		}
+
+		@Override
+		public String toString() {
+			return "Point [n=" + n + ", cnt=" + cnt + "]";
+		}
 	}
-	static int max,N,G,T,answer;
+
+	static int max, N, G, T, answer;
 	static boolean[] visited;
-	public static void main(String[] args) throws Exception{
+	static boolean flag;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		N = Integer.parseInt(st.nextToken());
 		T = Integer.parseInt(st.nextToken());
 		G = Integer.parseInt(st.nextToken());
-		
+
 		answer = Integer.MAX_VALUE;
+
 		visited = new boolean[100001];
-		
-		bfs(new Point(N,1));
-		
-		
-		if(answer<=T) {
+
+		flag = false;
+		bfs(new Point(N, 0));
+
+		//결과 출력
+		if (flag) {
 			System.out.println(answer);
-		}else {
+		} else {
 			System.out.println("ANG");
 		}
 	}
+
 	private static void bfs(Point start) {
 		Queue<Point> q = new LinkedList<>();
 		q.add(start);
 		visited[start.n] = true;
-		
-		while(!q.isEmpty()) {
+
+		while (!q.isEmpty()) {
 			Point p = q.poll();
 			int n = p.n;
-			if(n==G) {
-				answer= Math.min(answer, p.cnt);
+			// 탈출 성공했을 때
+			if (n == G) {
+				// T보다 버튼 수 작은지 확인
+				if (p.cnt <= T) {
+					flag = true;
+					answer = Math.min(answer, p.cnt);
+				}
 				return;
 			}
-			if(n+1 <100000 && !visited[n+1]) {
-				visited[n+1] = true;
-				q.add(new Point(n+1, p.cnt+1));
-			}			
-			int len = (int)(Math.log10(n*2)+1);
-			n = n*2 -(int)Math.pow(10, len-1);
-			System.out.println("n: "+n);
-			if(n!=0 && !visited[n]) {
-				visited[n] = true;
-				q.add(new Point(n,p.cnt+1));
+			// A버튼 눌렀을 때
+			if (n + 1 < 100000 && !visited[n + 1]) {
+				visited[n + 1] = true;
+				q.add(new Point(n + 1, p.cnt + 1));
 			}
-			
+
+			// n*2가 99999를 넘 는지 확인
+			n *= 2;
+			if (n > 99999) {
+				continue;
+			}
+			// n*2의 길이
+			int len = (int) (Math.log10(n) + 1);
+			// 가장 높은 자릿수 숫자 1 줄이기
+			n -= (int) Math.pow(10, len - 1);
+
+			// B버튼 눌렀을 때
+			if (n > 0 && !visited[n]) {
+				visited[n] = true;
+				q.add(new Point(n, p.cnt + 1));
+			}
 		}
-		
 	}
 }
