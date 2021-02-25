9692f896e5036b677748f944003a9cf36c578200
김영민 <kastori1990@gmail.com>
2021-02-26 00:25:17 +0900

    baek15991. 1, 2, 3 더하기 6

--- baek/baek15991.java
+++ baek/baek15991.java
@@ -3,23 +3,34 @@ package baek;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/*
+ * 1, 2, 3 더하기 6
+ * https://www.acmicpc.net/problem/15991
+ */
 public class baek15991 {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		for(int t= 0;t<T;t++) {
-			int N = Integer.parseInt(br.readLine());
-			
-			int[] dp = new int[N+1];
-			dp[1] = 1;
-			dp[2] = 2;
-			dp[3] = 2;  
-			
-			for(int i =4;i<=N;i++) {
-				dp[i] = dp[i/2]+1+dp[i-3]+dp[i-2]+dp[i-1];
-			}
-			System.out.println(dp[N]);
-			
+
+		StringBuilder answer = new StringBuilder();
+
+		long[] dp = new long[100_001];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 2;
+		dp[4] = 3;
+		dp[5] = 3;
+		dp[6] = 6;
+		
+		//d[n] = d[n-2] + d[n-4] + d[n-6];
+		for (int i = 7; i <= 100000; i++) {
+			dp[i] = (dp[i - 2] + dp[i - 4] + dp[i - 6]) % 1_000_000_009;
 		}
+		
+		for (int t = 0; t < T; t++) {
+			answer.append(dp[Integer.parseInt(br.readLine())]).append("\n");
+
+		}
+		System.out.println(answer.toString());
 	}
 }
