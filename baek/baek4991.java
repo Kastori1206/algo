c3abbfc6425ffe76a0d93951b2d6ea12904bab0e
김영민 <kastori1990@gmail.com>
2021-02-28 13:32:31 +0900

    baek4991. 로봇 청소기

--- baek/baek4991.java
+++ baek/baek4991.java
@@ -14,67 +14,137 @@ import java.util.StringTokenizer;
  */
 public class baek4991 {
 
-	static class Point{
-		int r,c;
+	static class Point {
+		int r, c;
 
 		public Point(int r, int c) {
 			super();
 			this.r = r;
 			this.c = c;
 		}
-		
+
+		@Override
+		public String toString() {
+			return "Point [r=" + r + ", c=" + c + "]";
+		}
+
 	}
+
 	static char[][] map;
+	static int[][] dist;
 	static boolean[][] visited;
-	static int R, C, answer;
-	static int[] dr = {-1,1,0,0};
-	static int[] dc = {0,0,-1,1};
-	static List<Point> dusts;
-	public static void main(String[] args) throws Exception{
+	static boolean[] isSelect;
+	static int R, C, answer, size;
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static Point[] dusts;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int C = Integer.parseInt(st.nextToken());
-		int R = Integer.parseInt(st.nextToken());
-		map = new char[R][C];
-		dusts = new ArrayList<>();
-		Point robot = null;
-		for(int r =0;r<R;r++) {
-			String temp = br.readLine();
-			for(int c =0;c<C;c++) {
-				char ch = temp.charAt(c);
-				if(ch=='o') {
-					robot = new Point(r,c);
-				}else if(ch=='*') {
-					dusts.add(new Point(r,c));
-				}				
-				map[r][c] = ch;				
+		StringTokenizer st;
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+			C = Integer.parseInt(st.nextToken());
+			R = Integer.parseInt(st.nextToken());
+			if (R == 0 && C == 0) {
+				break;
+			}
+			map = new char[R][C];
+			dusts = new Point[11];
+			dist = new int[11][11];
+			size = 1;
+			answer = Integer.MAX_VALUE;
+
+			for (int r = 0; r < R; r++) {
+				String temp = br.readLine();
+				for (int c = 0; c < C; c++) {
+					char ch = temp.charAt(c);
+					if (ch == 'o') {
+						dusts[0] = new Point(r, c);
+					} else if (ch == '*') {
+						dusts[size++] = new Point(r, c);
+					}
+					map[r][c] = ch;
+				}
+			}
+		
+			isSelect = new boolean[size];
+			if (isPossible()) {
+				permutation(0, 0, 0);
+				System.out.println(answer);
+			} else {
+				System.out.println(-1);
 			}
 		}
-		
-		
 	}
-	private static void bfs(Point start) {
-		visited = new boolean[R][C];
+
+	private static boolean isPossible() {
+		for (int i = 0; i < size; i++) {
+			for (int j = i + 1; j < size; j++) {
+				int d = bfs(dusts[i], dusts[j]);
+				if (d == -1) {
+					return false;
+				} else {
+					dist[i][j] = dist[j][i] = d;
+				}
+			}
+		}
+		return true;
+	}
+
+	private static int bfs(Point start, Point end) {
 		Queue<Point> q = new LinkedList<>();
+		visited = new boolean[R][C];
 		q.add(start);
 		visited[start.r][start.c] = true;
-		
-		while(!q.isEmpty()) {
+
+		int result = 0;
+		while (!q.isEmpty()) {
+			int qSize = q.size();
+			++result;
 			
+			while (qSize-- > 0) {
+				Point dust = q.poll();
+				for (int d = 0; d < 4; d++) {
+					int nr = dust.r + dr[d];
+					int nc = dust.c + dc[d];
+					
+					if (!isIn(nr, nc)) {
+						continue;
+					}
+					if (visited[nr][nc]) {
+						continue;
+					}
+					if (map[nr][nc] == 'x') {
+						continue;
+					}
+					if (nr == end.r && nc == end.c) {
+						return result;
+					}
+					visited[nr][nc] = true;
+					q.add(new Point(nr, nc));
+				}
+			}
 		}
+		return -1;
 	}
-	private static void permutation(int[] arr, int index, int n, boolean[] v) {
-		if(index == n) {
+
+	private static void permutation(int now, int cnt, int sum) {
+		if (cnt == size - 1) {
+			answer = Math.min(answer, sum);
 			return;
 		}
-		for(int i=0;i<n;i++) {
-			if(v[i]) {
+		for (int i = 1; i < size; i++) {
+			if (isSelect[i]) {
 				continue;
 			}
-			v[i] = true;
-			arr[index] = i;
-			permutation(arr, index+1, n, v);
-			v[i] = false;
+			isSelect[i] = true;
+			permutation(i, cnt + 1, sum + dist[now][i]);
+			isSelect[i] = false;
 		}
+	}
+
+	private static boolean isIn(int r, int c) {
+		return r >= 0 && c >= 0 && r < R && c < C;
 	}
 }
