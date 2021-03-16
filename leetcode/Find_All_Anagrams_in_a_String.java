fd48517253f739bbb1485a991e98fac5aaf0518a
김영민 <kastori1990@gmail.com>
2021-03-16 19:54:05 +0900

    438. Find All Anagrams in a String

--- leetcode/Find_All_Anagrams_in_a_String.java
+++ leetcode/Find_All_Anagrams_in_a_String.java
@@ -11,38 +11,38 @@ public class Find_All_Anagrams_in_a_String {
 		String p = "abc";
 		System.out.println(new Solution().findAnagrams(s, p));
 	}
-	static class Solution {
-	    public List<Integer> findAnagrams(String s, String p) {
-	        int slen = s.length();
-	        int plen = p.length();
-	        int[] sArr = new int[26];
-	        int[] pArr = new int[26];
-	        
-	        List<Integer> result = new ArrayList<Integer>();
 
-	        if(slen<=0 || plen<=0) {
-	        	return result;
-	        }
-	        
-	        for(int i =0;i<plen;i++) {
-	        	char c= p.charAt(i);
-	        	pArr[(int)(c-'a')]++;
-	        }
-	        
-	        for (int i = 0; i < slen; i ++)
-	        {
-	            char c = s.charAt(i);
-	            sArr[(int)(c-'a')] ++;
-	            if (i>=plen){
-	                char c2 = s.charAt(i-plen);
-	                sArr[(int)(c2-'a')] --;
-	            }
-	            
-	            if (Arrays.equals(pArr,sArr))	            {
-	                result.add(i-plen+1);
-	            }
-	        }
-	        return result;
-	    }
+	static class Solution {
+		public List<Integer> findAnagrams(String s, String p) {
+			int slen = s.length();
+			int plen = p.length();
+			int[] sArr = new int[26];
+			int[] pArr = new int[26];
+
+			List<Integer> result = new ArrayList<Integer>();
+
+			if (slen <= 0 || plen <= 0) {
+				return result;
+			}
+
+			for (int i = 0; i < plen; i++) {
+				char c = p.charAt(i);
+				pArr[(int) (c - 'a')]++;
+			}
+
+			for (int i = 0; i < slen; i++) {
+				char c = s.charAt(i);
+				sArr[(int) (c - 'a')]++;
+				if (i >= plen) {
+					char c2 = s.charAt(i - plen);
+					sArr[(int) (c2 - 'a')]--;
+				}
+
+				if (Arrays.equals(pArr, sArr)) {
+					result.add(i - plen + 1);
+				}
+			}
+			return result;
+		}
 	}
 }
