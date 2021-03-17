979f52736d11c7d2a3c4461513ee7bbd512806ac
김영민 <kastori1990@gmail.com>
2021-03-17 23:24:31 +0900

    A

---
+++ codeforces/div2/round708/A.java
@@ -0,0 +1,17 @@
+package codeforces.div2.round708;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int t =0;t<T;t++) {
+			
+		}
+	}
+}
