68b5e299df7d93585303686ba37367def3f57942
김영민 <kastori1990@gmail.com>
2021-03-19 01:40:37 +0900

    A. Domino on Windowsill

--- codeforces/div2/round106/A.java
+++ codeforces/div2/round106/A.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+ * A. Domino on Windowsill
+ * https://codeforces.com/contest/1499/problem/A
+ */
 public class A {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,7 +15,36 @@ public class A {
 		
 		int T = Integer.parseInt(br.readLine());
 		for(int t =0;t<T;t++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
 			
+			int k1 = Integer.parseInt(st.nextToken());
+			int k2 = Integer.parseInt(st.nextToken());
+			
+			st = new StringTokenizer(br.readLine());
+			int w = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			int min = Math.min(k1, k2);
+			int max = Math.max(k1, k2);
+			
+			int white = min;
+			int black = n-max;
+			max -= min;
+			
+			if(max!=0) {
+				white += max/2;
+				black += max/2;
+			}		
+						
+	
+			if(black>= b && white>=w) {
+				System.out.println("YES");
+			}else {
+				System.out.println("NO");
+			}
 		}
+			
+		
 	}
 }
