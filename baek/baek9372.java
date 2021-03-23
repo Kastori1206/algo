05c6f57e8cefe5a6fd17648d6993ff4cf5461ec6
김영민 <kastori1990@gmail.com>
2021-03-24 03:03:29 +0900

    baek9372.상근이의 여행

---
+++ baek/baek9372.java
@@ -0,0 +1,39 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/*
+ * 상근이의 여행
+ * https://www.acmicpc.net/problem/9372
+ */
+public class baek9372 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int t =0;t<T;t++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			List<Integer> adj[] = new ArrayList[N];
+			for(int i =0;i<N;i++) {
+				adj[i] = new ArrayList<>();
+			}
+			for(int i =0;i<M;i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+				adj[a].add(b);
+				adj[b].add(a);				
+			}
+		}
+	}
+}
