9726e7833907fc9fea989829442801be1eb74b04
김영민 <kastori1990@gmail.com>
2021-03-25 01:13:45 +0900

    baek9372. 상근이의 여행

--- baek/baek9372.java
+++ baek/baek9372.java
@@ -3,7 +3,9 @@ package baek;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 /*
@@ -11,6 +13,9 @@ import java.util.StringTokenizer;
  * https://www.acmicpc.net/problem/9372
  */
 public class baek9372 {
+	static int N,M,answer;
+	static boolean[] visited;
+	static List<Integer> adj[];
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -19,11 +24,12 @@ public class baek9372 {
 		for(int t =0;t<T;t++) {
 			st = new StringTokenizer(br.readLine());
 			
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			
-			List<Integer> adj[] = new ArrayList[N];
-			for(int i =0;i<N;i++) {
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			visited = new boolean[N+1];
+			answer = 0;
+			adj = new ArrayList[N+1];
+			for(int i =0;i<=N;i++) {
 				adj[i] = new ArrayList<>();
 			}
 			for(int i =0;i<M;i++) {
@@ -33,6 +39,27 @@ public class baek9372 {
 				
 				adj[a].add(b);
 				adj[b].add(a);				
+			}			
+			bfs();
+			//방문한 지역이 N개이면 비행기수는 N-1
+			System.out.println(answer-1);
+		}
+		
+	}
+	private static void bfs() {
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(1);
+		visited[1] = true;
+		while(!q.isEmpty()) {
+			int country = q.poll();
+			answer++;
+			for(int i =0;i<adj[country].size();i++) {
+				int next = adj[country].get(i);
+				if(visited[next]) {
+					continue;
+				}
+				visited[next] = true;
+				q.add(next);
 			}
 		}
 	}
