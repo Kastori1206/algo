06df0380a01e953918e14af8489f30705dd1e79b
김영민 <kastori1990@gmail.com>
2021-04-10 12:09:13 +0900

    programmers. 보석쇼핑

--- programmers/보석쇼핑.java
+++ programmers/보석쇼핑.java
@@ -13,24 +13,24 @@ public class 보석쇼핑 {
 		for (int i = 0; i < 4; i++) {
 			System.out.println(Arrays.toString(solution(gems[i])));
 		}
-//	
-
-		System.out.println(Arrays.toString(solution(new String[] { "XYZ", "XYZ", "XYZ" })));
 	}
 
 	static public int[] solution(String[] gems) {
 		int[] answer = new int[2];
+		//구간 안에 보석의 수량 파악
 		Map<String, Integer> buy = new HashMap<>();
+		//보석의 종류
 		Set<String> jewelrys = new HashSet<>();
 
 		int len = gems.length;
+		//보석 종류 구하기
 		for (int i = 0; i < len; i++) {
 			if (jewelrys.contains(gems[i])) {
 				continue;
 			}
 			jewelrys.add(gems[i]);
 		}
-
+		//보석 종류가 하나이면 1,1		
 		if (jewelrys.size() == 1) {
 			answer[0] = 1;
 			answer[1] = 1;
@@ -39,10 +39,11 @@ public class 보석쇼핑 {
 
 		int start = 0;
 		int end = 0;
-		int index = len;
-		int min = Integer.MAX_VALUE;
-
-		while(start<len) {
+		int index = len;//최소 구간의 처음 시작점
+		int min = Integer.MAX_VALUE; //구간의 최소 길이
+		
+		while (start < len) {
+			//구간의 길이가 최소값 보다 작고 끝점이 총길이만큼 갔을 때 
 			if (end - start + 1 > min || end == len) {
 				String jewelry = gems[start];
 				if (buy.get(jewelry) == 1) {
@@ -52,17 +53,20 @@ public class 보석쇼핑 {
 
 				}
 				start++;
-
-			} else {
+			} else {//아니면 끝점을 늘리고 보석 수 추가
 				String jewelry = gems[end];
 				buy.merge(jewelry, 1, (existValue, initValue) -> existValue + initValue);
 				end++;
 			}
+			//구간안에 보석이 다 있는지 확인
 			if (buy.size() == jewelrys.size()) {
+				//구간의 길이가 최소구간보다 작으면
 				if (min > end - start) {
 					min = end - start;
 					index = start;
-				} else if (end - start == min) {
+				}
+				//같으면 최소 구간 인덱스 확인(작은 값 찾기)
+				else if (end - start == min) {
 					if (index > start) {
 						index = start;
 					}
@@ -72,14 +76,5 @@ public class 보석쇼핑 {
 		answer[0] = index + 1;
 		answer[1] = answer[0] + min - 1;
 		return answer;
-	}
-
-	static boolean check(int[] count) {
-		for (int i = 0; i < count.length; i++) {
-			if (count[i] == 0) {
-				return false;
-			}
-		}
-		return true;
 	}
 }
