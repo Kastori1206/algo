7ad2ff762354793456a89ce31f7651deb5c71d92
김영민 <kastori1990@gmail.com>
2021-05-09 21:13:09 +0900

    baek9177. 단어 섞기

--- baek/baek9177.java
+++ baek/baek9177.java
@@ -10,14 +10,13 @@ import java.io.InputStreamReader;
 public class baek9177 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
 			String[] strs = br.readLine().split(" ");
 			int R = strs[0].length();
 			int C = strs[1].length();
-			for(String str : strs) {
-				System.out.println(str);
-			}
+			
 			boolean[][] dp = new boolean[R + 1][C + 1];
 			dp[0][0] = true;
 			for (int i = 1; i <= R; i++) {
@@ -31,7 +30,7 @@ public class baek9177 {
 					char aa = strs[0].charAt(r - 1);
 					char bb = strs[1].charAt(c - 1);
 					char cc = strs[2].charAt(r + c - 1);
-					if (aa != c && bb != cc) {
+					if (aa != cc && bb != cc) {
 						dp[r][c] = false;
 					} else if (aa == cc && bb != cc) {
 						dp[r][c] = dp[r - 1][c];
@@ -42,13 +41,7 @@ public class baek9177 {
 					}
 				}
 			}
-			
-			for(int r =0; r<=R;r++) {
-				for(int c =0;c<=C;c++) {
-					System.out.print(dp[r][c] +" ");
-				}
-				System.out.println();
-			}
+		
 			if (dp[R][C]) {
 				System.out.println("Data set " + (t + 1) + ": yes");
 			} else {
