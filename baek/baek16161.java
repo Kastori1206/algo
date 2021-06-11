accb1f93f067e5d7cdc7eee700cfaba457f8746e
김영민 <kastori1990@gmail.com>
2021-06-11 19:40:21 +0900

    baek16161. 가장 긴 증가하는 팰린드롬 부분수열

---
+++ baek/baek16161.java
@@ -0,0 +1,53 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 가장 긴 증가하는 팰린드롬 부분수열
+ * https://www.acmicpc.net/problem/16161
+ */
+public class baek16161 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int left = 0;
+		int right = 0;
+		int answer = 1;
+
+		while (left <= right && right < N - 1) {
+			if (arr[right] < arr[right + 1]) {
+				right++;
+			} else if (arr[right] == arr[right + 1]) {
+				for (int i = 0; i <= right - left; i++) {
+					if (right + 1 + i >= N || arr[right - i] != arr[right + 1 + i]) {
+						answer = Math.max(answer, i * 2);
+						break;
+					}
+				}
+				left = right + 1;
+				right++;
+			} else {
+				for (int i = 0; i < right - left; i++) {
+					if (right + 1 + i >= N || arr[right - 1 - i] != arr[right + 1 + i]) {
+						answer = Math.max(answer, 2 * i + 1);
+						break;
+					}
+				}
+				left = right + 1;
+				right++;
+			}
+		}
+		System.out.println(answer);
+	}
+}
