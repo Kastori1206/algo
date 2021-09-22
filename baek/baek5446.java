bb777c5c3b465270fb3ddfbd341273f3c89b1b15
김영민 <kastori1990@gmail.com>
2021-09-22 14:25:38 +0900

    baek5446.  용량 부족

--- baek/baek5446.java
+++ baek/baek5446.java
@@ -5,8 +5,10 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
 /**
- * 용량 부족
+ * 용량 부족 
  * https://www.acmicpc.net/problem/5446
  */
 public class baek5446 {
@@ -20,33 +22,34 @@ public class baek5446 {
 
 				node.childNodes.putIfAbsent(c, new Node());
 				node = node.childNodes.get(c);
-				node.flag = flag;
-				if (i == str.length() - 1) {
-					node.isLast = flag;
-					return;
-				}
+				node.del = flag;
+
+			}
+			if (flag) {
+				node.isLast = true;
 			}
 		}
-	
+
 	}
 
-	static class Node {
-		boolean flag;
-		boolean isLast;
-		Map<Character, Node> childNodes = new HashMap<>();
-	}
-
-	static void dfs(Node node) {		
-		if (node.flag) {
+	static void dfs(Node node) {
+		if (node.del) {
 			ans++;
 			return;
-		} else if (node.isLast) {
+		}
+		if (node.isLast) {
 			ans++;
 		}
 		for (Character key : node.childNodes.keySet()) {
 			dfs(node.childNodes.get(key));
 		}
+	}
 
+	static class Node {
+		boolean del;
+
+		boolean isLast;
+		Map<Character, Node> childNodes = new HashMap<>();
 	}
 
 	static int ans;
@@ -57,11 +60,10 @@ public class baek5446 {
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 0; t < T; t++) {
-			ans = 0;
 			int N = Integer.parseInt(br.readLine());
 			Trie trie = new Trie();
 			for (int i = 0; i < N; i++) {
-				trie.insert(br.readLine(),true);
+				trie.insert(br.readLine(), true);
 			}
 
 			int M = Integer.parseInt(br.readLine());
@@ -69,10 +71,10 @@ public class baek5446 {
 				trie.insert(br.readLine(), false);
 			}
 
+			ans = 0;
 			Node root = trie.root;
 			dfs(root);
-				
-			answer.append(ans == N ? 1 : ans);
+			answer.append(M == 0 ? 1 : ans).append("\n");
 		}
 		System.out.println(answer.toString());
 	}
