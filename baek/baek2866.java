10f7ef1f9c32a01754c9b2fe689ccfb6a3b6d12a
김영민 <kastori1990@gmail.com>
2021-11-23 22:51:49 +0900

    baekl2866. 문자열 잘라내기

---
+++ baek/baek2866.java
@@ -0,0 +1,67 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+/**
+ * 문자열 잘라내기
+ * https://www.acmicpc.net/problem/2866
+ */
+public class baek2866 {
+	static int R, C;
+	static char[][] map;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+
+		map = new char[R][C];
+
+		for (int r = 0; r < R; r++) {
+			String str = br.readLine();
+			for (int c = 0; c < C; c++) {
+				map[r][c] = str.charAt(c);
+			}
+		}
+
+		System.out.println(binarySearch());
+	}
+
+	static int binarySearch() {
+		int res = C - 1;
+		int l = 1, r = C - 1;
+		while (l <= r) {
+			int mid = (l + r) / 2;
+			if (check(mid)) {
+				r = mid - 1;
+				res = Math.min(res, mid);
+			} else {
+				l = mid + 1;
+			}
+		}
+		return res;
+	}
+
+	static boolean check(int start) {
+		Set<String> set = new HashSet<>();
+
+		for (int c = 0; c < C; c++) {
+			String str = "";
+			for (int r = start + 1; r < R; r++) {
+				str += map[r][c];
+			}
+			if (set.contains(str)) {
+				return true;
+			} else {
+				set.add(str);
+			}
+		}
+		return false;
+	}
+}
