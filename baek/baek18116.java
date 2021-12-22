48f8edc6ad40cf7459beb6f141eac5839b4185c2
Kim Young Min <Kastori1990@gmail.com>
2021-12-22 21:12:13 +0900

    baek18116. 로봇 조립

--- baek/baek18116.java
+++ baek/baek18116.java
@@ -2,7 +2,6 @@ package baek;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -27,6 +26,7 @@ public class baek18116 {
             p[i] = i;
             cnt[i] = 1;
         }
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             String cmd = st.nextToken();
