0b7930bf67886c2f08544fc9da473cb67021a542
Kim Young Min <Kastori1990@gmail.com>
2022-02-06 21:59:33 +0900

    programmers. 양궁대회

--- programmers/양궁대회.java
+++ programmers/양궁대회.java
@@ -21,12 +21,14 @@ public class 양궁대회 {
         answer = new int[11];
 
         dfs(0, n, 0, new int[n], info);
+        //max == 0이면 한번도 라이언이 이긴적이 없음
         return max==0? new int[]{-1} : answer;
     }
-
+    //중복조합 사용 11H10 = 184756
     public static void dfs(int start, int r, int idx, int[] result, int[] info) {
         if (idx == r) {
             int[] info2 = new int[11];
+            //과녁 점수를 어피치와 같이 과녁점수 맞춘 갯수로 표현
             for (int score : result) {
                 info2[10 - score]++;
             }
@@ -35,12 +37,18 @@ public class 양궁대회 {
             int apeach = 0;
 
             for (int i = 0; i < 11; i++) {
+                //info[0] = 10점 , info[1] = 9점 맞추기위해 score 계산
                 int score = 10 - i;
+                //라이언이 이긴 경우
                 if (info2[i] > info[i]) {
                     ryan += score;
-                } else if (info2[i] < info[i]) {
+                }
+                //어피치가 이긴 경우
+                else if (info2[i] < info[i]) {
                     apeach += score;
-                } else if (info[i] != 0 && info2[i] == info[i]) {
+                }
+                //과년에 맞추고 어피치와 라이언이 같은 갯수를 쏜 경우
+                else if (info[i] != 0 && info2[i] == info[i]) {
                     apeach += score;
                 }
             }
@@ -53,6 +61,7 @@ public class 양궁대회 {
         }
 
         for (int i = start; i < 11; i++) {
+            //라이언이 쏜 과녁 점수 저장
             result[idx] = i;
             dfs(i, r ,idx+1, result, info);
         }
