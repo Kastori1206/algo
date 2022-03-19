dd71865f3a319827000a8043fdbcdb5cd314550f
YoungMin Kim <45520209+Kastori1206@users.noreply.github.com>
2022-03-19 23:35:54 +0900

    baek18404. 현명한 나이트

---
+++ baek/baek18404.java
@@ -0,0 +1,9 @@
+package baek;
+
+public class baek18404 {
+	static int[] dr = {-2, -1, 1, 2, 2, 1, -1, -2};
+	static int[] dc = {1, 2, 2, 1, -1, -2, -2, -1};
+	public static void main(String[] args) {
+
+	}
+}
