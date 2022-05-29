ada8a104dd3941b339376ec8d2843dec96bae9d8
Kim Young Min <kastori1990@gmail.com>
2022-05-30 00:07:13 +0900

    baek17265. 나의 인생에는 수학과 함께

--- baek/baek17265.java
+++ baek/baek17265.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
  */
 public class baek17265 {
     static char[][] map;
-    static int N, MAX = Integer.MIN_VALUE, MIN = Integer.MAX_VALUE;
+    static int N, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
     static int[] dr = {0, 1};
     static int[] dc = {1, 0};
     public static void main(String[] args) throws Exception {
@@ -26,5 +26,45 @@ public class baek17265 {
                 map[r][c] = st.nextToken().charAt(0);
             }
         }
+        dfs(0, 0, map[0][0] - '0', ' ');
+        System.out.println(max + " " + min);
+    }
+
+    static void dfs(int r, int c, int res, char value) {
+        if (r == N - 1 && c == N - 1) {
+            max = Math.max(res, max);
+            min = Math.min(res, min);
+            return;
+        }
+        for (int d = 0; d < 2; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+
+            if (!isIn(nr, nc)) {
+                continue;
+            }
+            if (Character.isDigit(map[nr][nc])) {
+                dfs(nr, nc, calc(res, map[nr][nc] - '0', value), value);
+            } else {
+                dfs(nr, nc, res, map[nr][nc]);
+            }
+        }
+    }
+    static int calc(int x, int y, char op) {
+        switch (op) {
+            case '+':
+                return x + y;
+            case '-':
+                return x - y;
+            case '*':
+                return x * y;
+            case '/':
+                return x / y;
+        }
+        return -1;
+    }
+
+    static boolean isIn(int r, int c) {
+        return r >= 0 && c >= 0 && r < N && c < N;
     }
 }
