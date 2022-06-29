06b5504efedc8918f5200e91e6bb0d95a71160f3
Kim Young Min <kastori1990@gmail.com>
2022-06-29 10:34:52 +0900

    baek2098. 외판원 순회

--- baek/baek2098.java
+++ baek/baek2098.java
@@ -10,51 +10,53 @@ import java.util.StringTokenizer;
  * https://www.acmicpc.net/problem/2098
  */
 public class baek2098 {
-	static final int INF = 16*1_000_000;
-	static int N;
-	static int[][] W, dp;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		W  = new int[N][N];
-		dp = new int[N][(1<<N)-1];
-		for(int i =0;i<N;i++) {
-			Arrays.fill(dp[i], INF);
-		}
-		
-		for(int r =0;r<N;r++) {
-			st = new StringTokenizer(br.readLine());
-			for(int c =0;c<N;c++) {
-				W[r][c] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		System.out.println(tsp(0,1));
-		
-	}
-	private static int tsp(int node, int visited) {
-		if(visited==(1<<N) -1) {
-			if(W[node][0] == 0) {
-				return INF;
-			}
-			return W[node][0];
-		}
-		
-		if(dp[node][visited] !=INF) {
-			return dp[node][visited];
-		}
-		
-		for(int i =0;i<N;i++) {
-			if(W[node][i] == 0 || (visited &(1<<i)) !=0) {
-				continue;
-			}
-			
-			int next = visited | (1<<i);
-			dp[node][visited] = Math.min(dp[node][visited], tsp(i, next)+ W[node][i]);
+    static final int INF = 16 * 1_000_000;
+    static int N;
+    static int[][] W, dp;
 
-		}
-		return dp[node][visited];
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        W = new int[N][N];
+        dp = new int[N][(1 << N) - 1];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        for (int r = 0; r < N; r++) {
+            st = new StringTokenizer(br.readLine());
+            for (int c = 0; c < N; c++) {
+                W[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.println(tsp(0, 1));
+    }
+
+    private static int tsp(int node, int visited) {
+        if (visited == (1 << N) - 1) {
+            if (W[node][0] == 0) {
+                return INF;
+            }
+            return W[node][0];
+        }
+        if (dp[node][visited] != -1) {
+            return dp[node][visited];
+        }
+
+        dp[node][visited] = INF;
+
+        for (int i = 0; i < N; i++) {
+            if (W[node][i] == 0 || (visited & (1 << i)) != 0) {
+                continue;
+            }
+
+            int next = visited | (1 << i);
+            dp[node][visited] = Math.min(dp[node][visited], tsp(i, next) + W[node][i]);
+
+        }
+        return dp[node][visited];
+    }
 }
