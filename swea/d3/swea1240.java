868e78f4cdc12dd2af0c7966c9d9781fbea8d984
Kim Young Min <kastori1990@gmail.com>
2022-07-19 20:27:36 +0900

    swea1240. 단순 2진 암호코드

--- swea/d3/swea1240.java
+++ swea/d3/swea1240.java
@@ -3,76 +3,71 @@ package swea.d3;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 /*
  * [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드 
  */
+
 public class swea1240 {
-	public static void main (String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int T = Integer.parseInt(st.nextToken());
-		for(int t = 1 ;t<=T;t++) {			
-			HashMap<String, Integer> maps = new HashMap<>();
-			maps.put("0001101", 0);
-			maps.put("0011001", 1);
-			maps.put("0010011", 2);
-			maps.put("0111101", 3);
-			maps.put("0100011", 4);
-			maps.put("0110001", 5);
-			maps.put("0101111", 6);
-			maps.put("0111011", 7);
-			maps.put("0110111", 8);
-			maps.put("0001011", 9);
-			
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		Map<String, Integer> map = new HashMap<>();
+		map.put("0001101", 0);
+		map.put("0011001", 1);
+		map.put("0010011", 2);
+		map.put("0111101", 3);
+		map.put("0100011", 4);
+		map.put("0110001", 5);
+		map.put("0101111", 6);
+		map.put("0111011", 7);
+		map.put("0110111", 8);
+		map.put("0001011", 9);
+
+		for (int t = 0; t < T; t++) {
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
-			
+
+			String str = "";
 			int end = 0;
-			
-			String[] map = new String[N];
-			for(int i =0;i<N;i++) {
-				map[i] = br.readLine();				
-				
-			}			
-			String temp = null;
-			boolean flag = false;
-			for(int i =0;i<N;i++) {
-				if(map[i].contains("1")) {
-					temp = map[i];					
-					for(int j=0;j<M;j++) {
-						if(map[i].charAt(j)=='1') {
-							flag = true;
-							end = j;
-						}
-					}	
+			for (int i = 0; i < N; i++) {
+				String input = br.readLine();
+				for (int j = M-1; j >=0; j--) {
+					if (input.charAt(j) == '1') {
+						str = input;
+						end = j;
+						break;
+					}
 				}
-				if(flag) {
-					break;
-				}				
 			}
-			int start = end-7*8;
+			int start = end - 7 * 8;
+			int odd = 0;
+			int even = 0;
+			int check = 0;
+			for (int i = 0; i < 8; i++) {
+				int num = map.get(str.substring(7 * i + start + 1, 7 * i + start + 1 + 7));
+				if (i == 7) {
+					check = num;
+				}else if (i % 2 == 0) {
+					odd += num;
+				} else {
+					even += num;
+				}
+			}
 			int answer = 0;
-			int[] result = new int[8+1]; 
-			
-			for(int i =0;i<8;i++) {				
-				result[i+1] = maps.get(temp.substring(7*i+start+1, 7*i+start+7+1 )); 
+			if (((odd * 3) + even + check) % 10 == 0) {
+				answer = odd + even + check;
 			}
-			int sum1 =0;//홀수
-			int sum2 =0;//짝수
-			for(int i =1;i<8;i++) {
-				if(i%2==1) {
-					sum1 += result[i];
-				}else {
-					sum2 += result[i];
-				}
-			}
-			if(((sum1*3)+sum2 +result[8])%10 ==0) {
-				answer = sum1+sum2+result[8];
-			}
-			System.out.println(String.format("#%d %d",t,answer));
-		}//test_case
-	}//main
+
+			sb.append(String.format("#%d %d\n", t + 1, answer));
+
+		}
+		System.out.println(sb);
+	}
 }
