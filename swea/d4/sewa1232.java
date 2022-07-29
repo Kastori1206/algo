2279bdef48cdc7d0a2f85e01d78e6d8ecbea565d
Kim Young Min <kastori1990@gmail.com>
2022-07-29 21:04:03 +0900

    swea1232. 사칙연산

---
+++ swea/d4/sewa1232.java
@@ -0,0 +1,68 @@
+package swea.d4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/**
+ *  1232. [S/W 문제해결 기본] 9일차 - 사칙연산 D4
+ */
+public class sewa1232 {
+    static int[][] tree;
+    static String[] ops;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+
+
+        for (int t = 1; t <= 10; t++) {
+            int N = Integer.parseInt(br.readLine());
+
+            tree = new int[N + 1][2];
+            ops = new String[N + 1];
+
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int v = Integer.parseInt(st.nextToken());
+                String input = st.nextToken();
+                ops[v] = input;
+                if (input.equals("-") || input.equals("+") || input.equals("/") || input.equals("*")) {
+                    int a = Integer.parseInt(st.nextToken());
+                    int b = Integer.parseInt(st.nextToken());
+
+                    tree[v][0] = a;
+                    tree[v][1] = b;
+                }
+            }
+
+            double answer = dfs(1);
+            sb.append(String.format("#%d %d\n", t, (int)answer));
+        }
+        System.out.println(sb);
+    }
+    static double dfs(int cur) {
+        if (tree[cur][0] != 0) {
+            double a = dfs(tree[cur][0]);
+            double b = dfs(tree[cur][1]);
+
+            return calc(a, b, ops[cur]);
+        }
+        return Double.parseDouble(ops[cur]);
+    }
+
+    static double calc(double a, double b, String op){
+        switch (op) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+        }
+        return -1;
+    }
+}
