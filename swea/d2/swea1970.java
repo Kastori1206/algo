1757740a4f1ee7227359f76629c7a35ddd8da450
Kim Young Min <kastori1990@gmail.com>
2022-07-30 14:38:00 +0900

    swea1970. 쉬운 거스름돈

--- swea/d2/swea1970.java
+++ swea/d2/swea1970.java
@@ -1,28 +1,34 @@
 package swea.d2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 import javax.swing.plaf.synth.SynthSeparatorUI;
 
 public class swea1970 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
 		int[]a = {50000,10000,5000,1000,500,100,50,10};
 
 		for(int test_case = 1; test_case <= T; test_case++){
-			int N = sc.nextInt();
-			String result ="";
+			int N = Integer.parseInt(br.readLine());
+			StringBuilder result = new StringBuilder();
 			
 			for(int i =0;i<a.length;i++){
-				result += (N/a[i])+" ";
+				result.append(N / a[i]).append(" ");
 				N%=a[i];
 			}
-			System.out.println("#"+test_case);
-			System.out.println(result);		
-		}
 
+			sb.append(String.format("#%d\n%s\n", test_case, result.toString()));
+		}
+		System.out.println(sb);
 	}
 
 }
