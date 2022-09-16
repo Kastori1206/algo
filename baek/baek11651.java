384e2f1b69775b248044616a61da80fdd7649821
Kim Young Min <kastori1990@gmail.com>
2022-09-16 10:16:09 +0900

    baek11651. 좌표 정렬하기 2

---
+++ baek/baek11651.java
@@ -0,0 +1,42 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ * 좌표 정렬하기 2
+ * https://www.acmicpc.net/problem/11651
+ */
+public class baek11651 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return Integer.compare(o1[0], o2[0]);
+            }
+            return Integer.compare(o1[1], o2[1]);
+        });
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            pq.offer(new int[]{a, b});
+        }
+        StringBuilder sb = new StringBuilder();
+
+        while (!pq.isEmpty()) {
+            int[] temp = pq.poll();
+            sb.append(temp[0]).append(" ").append(temp[1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
+
