0bc608a1a5c9682b3cafadc6ca0fb30e2040112e
Kim Young Min <kastori1990@gmail.com>
2022-10-14 14:55:29 +0900

    연속 부분 수열 합의 개수

--- programmers/연속_부분_수열_합의_개수.java
+++ programmers/연속_부분_수열_합의_개수.java
@@ -12,17 +12,18 @@ public class 연속_부분_수열_합의_개수 {
         int[] elements = {7, 9, 1, 1, 4};
         System.out.println(solution(elements));
     }
+
     static public int solution(int[] elements) {
         Set<Integer> set = new HashSet<>();
         int n = elements.length;
         int[] d = new int[n * 2];
         d[0] = elements[0];
-        for (int i = 1; i < n*2; i++) {
+        for (int i = 1; i < n * 2; i++) {
             d[i] = d[i - 1] + elements[i % n];
         }
 
         for (int i = 0; i < n * 2; i++) {
-            for (int j = i%n; j < i; j++) {
+            for (int j = i % n; j < i; j++) {
                 set.add(d[i] - d[j]);
             }
         }
